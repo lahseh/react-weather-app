04ddb90fc66f18e2374692e2e4ec040d0e6a2989
lahseh <orisaleyeblessing@gmail.com>
2024-02-21 23:55:32 +0000

    project structure done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Blessing Orisaleye and is{" "}
+        <a href="https://github.com/lahseh/react-weather" target="_blank">
+          open-sourced on GitHub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
