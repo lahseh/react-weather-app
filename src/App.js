a396a214d55ba8cc3c5028b6d18b1d5c158a8b83
lahseh <orisaleyeblessing@gmail.com>
2024-03-02 01:18:52 +0000

    refactored code and fixed warnings

--- src/App.js
+++ src/App.js
@@ -6,18 +6,27 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="London" />
 
         <footer>
           This project was coded by Blessing Orisaleye and is{" "}
-          <a href="https://github.com/lahseh/react-weather-app" target="_blank">
+          <a
+            href="https://github.com/lahseh/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             open-sourced on GitHub{" "}
           </a>{" "}
           and{" "}
-          <a href="https://wondrous-crepe-301456.netlify.app/" target="_blank">
+          <a
+            href="https://wondrous-crepe-301456.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             {" "}
-            hosted on Netlify{" "}
+            hosted on Netlify
           </a>
+          .
         </footer>
       </div>
     </div>
