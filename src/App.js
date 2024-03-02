f148aaccec4fedb91ea04754acc2af750e3f1b4e
lahseh <orisaleyeblessing@gmail.com>
2024-03-02 01:09:47 +0000

    added netlify link

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ export default function App() {
         <footer>
           This project was coded by Blessing Orisaleye and is{" "}
           <a href="https://github.com/lahseh/react-weather-app" target="_blank">
-            open-sourced on GitHub
+            open-sourced on GitHub{" "}
+          </a>{" "}
+          and{" "}
+          <a href="https://wondrous-crepe-301456.netlify.app/" target="_blank">
+            {" "}
+            hosted on Netlify{" "}
           </a>
         </footer>
       </div>
